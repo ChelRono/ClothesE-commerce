ef7e263f8cda17455e67d726af0a393a03cffb4b
Valarie <Valarierono97@gmail.com>
2023-09-09 17:56:35 +0300

    Added names and prices to explore page

--- app/src/main/java/com/example/clothesecommerceapp/BottomNavContentScreens.kt
+++ app/src/main/java/com/example/clothesecommerceapp/BottomNavContentScreens.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,9 +16,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -25,9 +31,13 @@ import androidx.compose.material.ButtonColors
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -41,8 +51,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -81,7 +93,7 @@ fun MainContent(){
         Column(
             modifier = Modifier
                 .fillMaxHeight()
-                .padding(horizontal = 58.dp, vertical = 130.dp),
+                .padding(horizontal = 58.dp, vertical = 100.dp),
             verticalArrangement = Arrangement.Bottom
 
 
@@ -109,7 +121,7 @@ fun TextOne(){
         text ="Make Your",
         color=Color.White,
         fontWeight = FontWeight.Bold,
-        fontSize = 28.sp
+        fontSize = 30.sp
 
         )
 }
@@ -120,7 +132,7 @@ fun TextTwo(){
         text ="purchases as",
         color=Color.White,
         fontWeight = FontWeight.Bold,
-        fontSize = 28.sp
+        fontSize = 30.sp
 
     )
 }
@@ -140,7 +152,10 @@ fun ButtonText(){
             colors = androidx.compose.material3.ButtonDefaults.buttonColors(Color.White)
         ) {
             Text(
-                text = "Women", color = Color.Black)
+                text = "WOMEN",
+                color = Color.Black,
+                fontSize = 18.sp
+                )
 
         }
         Spacer(modifier = Modifier.width(30.dp))
@@ -155,7 +170,10 @@ fun ButtonText(){
             onClick = {}
         ) {
             Text(
-                text = "MEN", color = Color.White)
+                text = "MEN",
+                color = Color.White,
+                fontSize = 18.sp
+                )
 
         }
         }
@@ -167,7 +185,134 @@ fun ButtonText(){
 @Composable
 fun ExploreScreen() {
 
+    val clothes = listOf(
+        R.drawable.img_2,
+        R.drawable.img_3,
+        R.drawable.img_1,
+        R.drawable.img_5,
+        R.drawable.img_6,
+        R.drawable.img_7,
+        R.drawable.img_8,
+        R.drawable.img_4,
+
+
+    )
+    val list: List<String> = listOf(
+        "Sweater",
+        "Jacket",
+        "Dress",
+        "Jeans",
+        "Kimono",
+        "Skirt",
+        "Scarf",
+        "Sunhat",
+    )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text="Search",
+                        textAlign = TextAlign.Center
+                        )},
+                navigationIcon ={
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .height(20.dp)
+                            .padding(horizontal = 24.dp)
+                    ){
+                        Icon(imageVector = Icons.Default.KeyboardArrowLeft, contentDescription = "Back")
+                        Text(
+                            "Explore",
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.weight(1f))
+                        Icon(imageVector = Icons.Default.Search, contentDescription = "Delete")
+
+
+
+
+                    } } ,
+                modifier=Modifier
+
+                    .background (color=Color.White)
+            )
+
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(paddingValues)
+        ) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = Modifier.padding(10.dp)
+            ) {
+                items(clothes.size) {
+                    ClothItem(imageId = clothes[it], title = "Who Ate My Cheese")
+
+                    BottomText(title =list[it])
+
+                }
+            }
+        }
     }
+}
+
+@Composable
+fun ClothItem(imageId: Int, title: String) {
+    Card(
+        modifier = Modifier
+            .padding(horizontal = 6.dp, vertical = 16.dp)
+            .size(250.dp)
+            .border(2.dp, color = Color.White, shape = RoundedCornerShape(16.dp))
+            .clickable {
+
+            },
+        elevation = CardDefaults.cardElevation()
+    ) {
+        Column(
+            Modifier.padding(5.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+                    Image(
+                        painter = painterResource(id = imageId),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(5.dp),
+                        contentScale = ContentScale.FillBounds
+                    )
+                }
+        }
+    }
+
+@Composable
+fun BottomText( title: String){
+    Column(
+        modifier=Modifier
+            .padding(horizontal = 24.dp)
+    ) {
+        Spacer(modifier = Modifier.height(270.dp))
+        Text(
+            text=title,
+            fontSize = 18.sp,
+            color = Color.Black,
+            fontWeight = FontWeight.Bold
+
+            )
+        Text(
+            text="$10",
+            fontSize = 18.sp,
+            color = Color.Black,
+        )
+    }
+}
 
 @Composable
 fun ShopScreen() {
